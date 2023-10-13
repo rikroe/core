764ad5f6cd269d442f876d4f9619955ac3d17570
rikroe <rikroe@users.noreply.github.com>
2023-10-14 00:23:01 +0200

    Add set_datetime & set_schedule services

--- homeassistant/components/cometblue/__init__.py
+++ homeassistant/components/cometblue/__init__.py
@@ -1,20 +1,29 @@
 """Comet Blue Bluetooth integration."""
 from __future__ import annotations
 
+from datetime import datetime
 import logging
 from uuid import UUID
 
 from bleak import BleakError
 import cometblue
+import voluptuous as vol
 
 from homeassistant.components import bluetooth
+from homeassistant.components.schedule import (
+    CONF_ALL_DAYS,
+    TIME_RANGE_SCHEMA,
+    valid_schedule,
+)
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_ADDRESS, CONF_PIN, Platform
-from homeassistant.core import HomeAssistant
+from homeassistant.const import CONF_ADDRESS, CONF_ENTITY_ID, CONF_PIN, Platform
+from homeassistant.core import HomeAssistant, ServiceCall, SupportsResponse
 from homeassistant.exceptions import ConfigEntryNotReady
+import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.entity_registry import EntityRegistry
 
-from .const import CONF_DEVICE_NAME, DOMAIN
+from .const import CONF_DATETIME, CONF_DEVICE_NAME, CONF_SCHEDULE, DOMAIN
 from .coordinator import CometBlueDataUpdateCoordinator
 
 PLATFORMS: list[Platform] = [
@@ -24,6 +33,19 @@ PLATFORMS: list[Platform] = [
 ]
 LOGGER = logging.getLogger(__name__)
 TIMEOUT = 10
+
+
+SERVICE_BASE_SCHEMA = {vol.Required(CONF_ENTITY_ID): cv.entity_id}
+SERVICE_SET_DATETIME_SCHEMA = {
+    vol.Optional(CONF_DATETIME): cv.datetime,
+}
+SCHEDULE_SCHEMA = {
+    vol.Optional(day): vol.All([TIME_RANGE_SCHEMA], valid_schedule)
+    for day in CONF_ALL_DAYS
+}
+SERVICE_SCHEDULE_SCHEMA = {
+    vol.Required(CONF_SCHEDULE): SCHEDULE_SCHEMA,
+}
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -74,7 +96,62 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
+    async def service_set_datetime(service_call: ServiceCall) -> None:
+        """Service call to update the datetime on the device."""
+
+        target_datetime = service_call.data.get("datetime") or datetime.now()
+
+        for entity_id in service_call.data["entity_id"]:
+            entity_coordinator = await get_coordinator_for_service(hass, entity_id)
+
+            await entity_coordinator.send_command(
+                "set_datetime_async",
+                {"date": target_datetime},
+                service_call.service,
+            )
+
+    async def service_set_schedule(service_call: ServiceCall) -> None:
+        """Service call to update the datetime on the device."""
+
+        for entity_id in service_call.data["entity_id"]:
+            entity_coordinator = await get_coordinator_for_service(hass, entity_id)
+            for day in service_call.data["schedule"]:
+                LOGGER.info(
+                    "%s (%s): %s - %s",
+                    entity_id,
+                    entity_coordinator.device,
+                    day,
+                    service_call.data["schedule"][day],
+                )
+
+    hass.services.async_register(
+        DOMAIN,
+        "set_datetime",
+        service_set_datetime,
+        schema=cv.make_entity_service_schema(SERVICE_SET_DATETIME_SCHEMA),
+        supports_response=SupportsResponse.NONE,
+    )
+    hass.services.async_register(
+        DOMAIN,
+        "set_schedule",
+        service_set_schedule,
+        schema=cv.make_entity_service_schema(SERVICE_SCHEDULE_SCHEMA),
+        supports_response=SupportsResponse.NONE,
+    )
+
     return True
+
+
+async def get_coordinator_for_service(
+    hass: HomeAssistant, entity_id: str
+) -> CometBlueDataUpdateCoordinator:
+    """Return the coordinator for a given entity_id."""
+    er = EntityRegistry(hass)
+    await er.async_load()
+    entity = er.async_get(entity_id)
+    if not entity:
+        raise ValueError(f"Entity '{entity_id}' not found")
+    return hass.data[DOMAIN][entity.config_entry_id]
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
--- homeassistant/components/cometblue/const.py
+++ homeassistant/components/cometblue/const.py
@@ -4,3 +4,5 @@ DOMAIN = "cometblue"
 DEFAULT_NAME = "Eurotronic CometBlue"
 
 CONF_DEVICE_NAME = "device_name"
+CONF_DATETIME = "datetime"
+CONF_SCHEDULE = "schedule"
